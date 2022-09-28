4f583c59472c69c2ae0423dbda789f3fd8ae55be
Apodlubny <apodlubny@gmail.com>
2022-09-28 21:13:07 +0300

    main

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 18px;
+  color: rgb(1, 1, 1);
+`;
